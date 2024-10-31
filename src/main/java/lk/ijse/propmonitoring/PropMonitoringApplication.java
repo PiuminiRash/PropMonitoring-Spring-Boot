b806470be1669f87aba89985e1855989c4c4b261
PiuminiRash <piuminirashmika@gmail.com>
2024-11-01 00:26:12 +0530

    Init

---
+++ src/main/java/lk/ijse/propmonitoring/PropMonitoringApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.propmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/propmonitoring/PropMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.propmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
