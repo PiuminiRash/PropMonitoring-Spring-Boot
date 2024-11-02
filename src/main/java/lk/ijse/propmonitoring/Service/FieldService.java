e65dbcf81c40a18c5758972d66ce4247ed2fd4db
PiuminiRash <piuminirashmika@gmail.com>
2024-11-02 19:11:24 +0530

    Implement All Main Service

---
+++ src/main/java/lk/ijse/propmonitoring/Service/FieldService.java
@@ -0,0 +1,4 @@
+package lk.ijse.propmonitoring.Service;
+
+public interface FieldService {
+}
---
+++ src/main/java/lk/ijse/propmonitoring/Service/UserService.java
@@ -0,0 +1,8 @@
+/* Created By Sithira Roneth
+* Date :10/29/24
+* Time :10:35
+* Project Name :Prop-Monitoring-System 
+* */
+package lk.ijse.propmonitoring.Service;
+public interface UserService {
+}
