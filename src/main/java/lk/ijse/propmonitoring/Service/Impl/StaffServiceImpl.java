3d008524dfb350db0a65060a5449e094a8a2ed55
PiuminiRash <piuminirashmika@gmail.com>
2024-11-02 19:12:58 +0530

    Build Main Service Impl

---
+++ src/main/java/lk/ijse/propmonitoring/Service/Impl/StaffServiceImpl.java
@@ -0,0 +1,9 @@
+/* Created By Sithira Roneth
+ * Date :10/29/24
+ * Time :10:33
+ * Project Name :Prop-Monitoring-System
+ * */
+package lk.ijse.propmonitoring.Service.Impl;
+
+public class StaffServiceImpl {
+}
