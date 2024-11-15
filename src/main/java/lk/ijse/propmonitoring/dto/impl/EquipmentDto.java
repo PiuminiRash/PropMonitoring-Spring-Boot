79d356f3a7ce918ed8ea43bf77fbe62613a254ac
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:08:51 +0530

    Update Equipment Dto

--- src/main/java/lk/ijse/propmonitoring/dto/impl/EquipmentDto.java
+++ src/main/java/lk/ijse/propmonitoring/dto/impl/EquipmentDto.java
@@ -1,20 +1,23 @@
 package lk.ijse.propmonitoring.dto.impl;
 
+import lk.ijse.propmonitoring.dto.EquipmentStatus;
 import lk.ijse.propmonitoring.entity.Status;
 import lk.ijse.propmonitoring.entity.Type;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EquipmentDto {
+public class EquipmentDto implements EquipmentStatus {
     private String equipmentId;
     private String equipmentName;
     private Type equipmentType;
     private Status status;
-    private StaffDto staff;
-    private FieldDto field;
+    private List<StaffDto> staff;
+    private List<FieldDto> field;
 
 }
