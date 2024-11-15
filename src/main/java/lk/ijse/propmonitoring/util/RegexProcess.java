ecb964699052fe6aae7cde8d7f920cbc8e98d7be
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:19:36 +0530

    Update Regex Process

---
+++ src/main/java/lk/ijse/propmonitoring/util/RegexProcess.java
@@ -0,0 +1,21 @@
+/* Created By Sithira Roneth
+ * Date :9/30/24
+ * Time :16:05
+ * Project Name :Note_Collector
+ * */
+package lk.ijse.propmonitoring.util;
+
+import java.util.regex.Pattern;
+
+public class RegexProcess {
+    public static boolean noteIdMatcher(String noteId) {
+        String regexForNoteId = "^Note[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForNoteId);
+        return regexPattern.matcher(noteId).matches();
+    }
+    public static boolean userIdMatcher(String userId) {
+        String regexForUserId = "^User[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForUserId);
+        return regexPattern.matcher(userId).matches();
+    }
+}
