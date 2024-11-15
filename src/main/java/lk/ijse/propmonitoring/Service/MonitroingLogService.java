e607f6a165fea8f698ffa6b7ec31faf60449e9a7
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:12:32 +0530

    Update Monitoring Service

--- src/main/java/lk/ijse/propmonitoring/Service/MonitroingLogService.java
+++ src/main/java/lk/ijse/propmonitoring/Service/MonitroingLogService.java
@@ -1,4 +1,14 @@
 package lk.ijse.propmonitoring.Service;
 
+import lk.ijse.propmonitoring.dto.MonitoringLogStatus;
+import lk.ijse.propmonitoring.dto.impl.MonitoringLogDto;
+
+import java.util.List;
+
 public interface MonitroingLogService {
+    void saveMonitor(MonitoringLogDto monitoringLogDto);
+    List<MonitoringLogDto> getAllMonitor();
+    MonitoringLogStatus getSelectedMonitor(String logCode);
+    void updateMonitor(String logCode,MonitoringLogDto monitoringLogDto);
+    void deleteMonitor(String logCode);
 }
