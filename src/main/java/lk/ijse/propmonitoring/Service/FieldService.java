5312afbc12022111a7fa0173a0b2b6c7298b0a9c
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:10:52 +0530

    Update Field Service

--- src/main/java/lk/ijse/propmonitoring/Service/FieldService.java
+++ src/main/java/lk/ijse/propmonitoring/Service/FieldService.java
@@ -1,4 +1,14 @@
 package lk.ijse.propmonitoring.Service;
 
+import lk.ijse.propmonitoring.dto.FieldStatus;
+import lk.ijse.propmonitoring.dto.impl.FieldDto;
+
+import java.util.List;
+
 public interface FieldService {
+    void saveField(FieldDto fieldDto);
+    List<FieldDto> getAllFields();
+    FieldStatus getSelectedField(String fieldCode);
+    void updateField(String fieldCode, FieldDto fieldDto);
+    void deleteField(String fieldCode);
 }
