084e0ba3733c1bb2f623a146236495ee4682ad19
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:22:12 +0530

    Update User Service Impl

--- src/main/java/lk/ijse/propmonitoring/Service/Impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/propmonitoring/Service/Impl/UserServiceImpl.java
@@ -1,7 +1,22 @@
-/* Created By Sithira Roneth
-* Date :10/29/24
-* Time :10:34
-* Project Name :Prop-Monitoring-System 
-* */
-package lk.ijse.propmonitoring.Service.Impl;public class UserServiceImpl {
+package lk.ijse.propmonitoring.Service.Impl;
+
+import lk.ijse.propmonitoring.Service.UserService;
+import lk.ijse.propmonitoring.dao.UserDao;
+import lk.ijse.propmonitoring.dto.impl.UserDto;
+import lk.ijse.propmonitoring.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void userSave(UserDto userDto) {
+
+    }
 }
