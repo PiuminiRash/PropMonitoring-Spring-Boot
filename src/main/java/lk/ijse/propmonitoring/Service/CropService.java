cc5fbc5db8a27e019368941c2029f539f10128b4
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:07:36 +0530

    Update Crop Service

--- src/main/java/lk/ijse/propmonitoring/Service/CropService.java
+++ src/main/java/lk/ijse/propmonitoring/Service/CropService.java
@@ -1,4 +1,15 @@
 package lk.ijse.propmonitoring.Service;
 
+import lk.ijse.propmonitoring.dto.CropStatus;
+import lk.ijse.propmonitoring.dto.impl.CropDto;
+
+import java.util.List;
+
 public interface CropService {
+    void saveCrop(CropDto cropDto);
+    CropStatus getSelectedCrop(String cropCode);
+    List<CropDto> getAllCrops();
+    void deleteCrop(String cropCode);
+    void updateCrop(String cropCode,CropDto cropDto);
+    String generateCropCode();
 }
