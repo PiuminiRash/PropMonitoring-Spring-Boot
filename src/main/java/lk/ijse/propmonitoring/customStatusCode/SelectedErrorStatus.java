c4ae204f9840aade725bdf3122c4285f3a8ba476
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:19:54 +0530

    Update Select Error Status

---
+++ src/main/java/lk/ijse/propmonitoring/customStatusCode/SelectedErrorStatus.java
@@ -0,0 +1,15 @@
+package lk.ijse.propmonitoring.customStatusCode;
+
+import lk.ijse.propmonitoring.dto.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//public class SelectedErrorStatus implements UserStatus, NoteStatus {
+public class SelectedErrorStatus implements CropStatus, VehicleStatus, EquipmentStatus, FieldStatus, StaffStatus, MonitoringLogStatus {
+    private int statusCode;
+    private String status;
+}
