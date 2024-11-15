cfff6a5710bb968e00d36df204cff288ff20522c
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:07:03 +0530

    Update Crop Dto

--- src/main/java/lk/ijse/propmonitoring/dto/impl/CropDto.java
+++ src/main/java/lk/ijse/propmonitoring/dto/impl/CropDto.java
@@ -5,19 +5,22 @@
 * */
 package lk.ijse.propmonitoring.dto.impl;
 
+import lk.ijse.propmonitoring.dto.CropStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CropDto {
+public class CropDto implements CropStatus {
     private String cropCode;
     private String cropName;
     private String scientificName;
     private String cropImage;
     private String category;
     private String season;
-    private FieldDto field;
+    private List<FieldDto> field;
 }
