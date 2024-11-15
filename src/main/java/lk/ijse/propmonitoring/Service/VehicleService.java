6fd0fe0697a3375454370deda55237d7d147de85
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:22:58 +0530

    Update Vehicle Service

--- src/main/java/lk/ijse/propmonitoring/Service/VehicleService.java
+++ src/main/java/lk/ijse/propmonitoring/Service/VehicleService.java
@@ -1,4 +1,15 @@
 package lk.ijse.propmonitoring.Service;
 
+import lk.ijse.propmonitoring.dto.VehicleStatus;
+import lk.ijse.propmonitoring.dto.impl.VehicleDto;
+
+import java.util.List;
+
 public interface VehicleService {
+    void saveVehicle(VehicleDto vehicleDto);
+    VehicleStatus getSelectedVehicle(String vehicleCode);
+    List<VehicleDto> getAllVehicles();
+    void updateVehicle(String vehicleCode,VehicleDto vehicleDto);
+    void deleteVehicle(String vehicleCode);
+    String generatedVehicleCode();
 }
