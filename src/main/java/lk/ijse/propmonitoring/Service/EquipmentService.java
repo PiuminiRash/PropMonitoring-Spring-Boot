9d50e03432ca9e2ffabca637ab33d67fd00a59a5
PiuminiRash <piuminirashmika@gmail.com>
2024-11-15 06:09:31 +0530

    Update Equipment Service

--- src/main/java/lk/ijse/propmonitoring/Service/EquipmentService.java
+++ src/main/java/lk/ijse/propmonitoring/Service/EquipmentService.java
@@ -1,4 +1,14 @@
 package lk.ijse.propmonitoring.Service;
 
+import lk.ijse.propmonitoring.dto.EquipmentStatus;
+import lk.ijse.propmonitoring.dto.impl.EquipmentDto;
+
+import java.util.List;
+
 public interface EquipmentService {
+    void saveEquipment(EquipmentDto equipmentDto);
+    EquipmentStatus getSelectedEquipment(String equipmentId);
+    List<EquipmentDto> getAllEquipment();
+    void updateEquipment(String equipmentId, EquipmentDto equipmentDto);
+    void deleteEquipment(String equipmentId);
 }
