b0bfecd148ad33e0f5ad061f96c07b3cf412aba6
PiuminiRash <piuminirashmika@gmail.com>
2024-11-20 11:52:46 +0530

    update Main method

--- src/main/java/lk/ijse/propmonitoring/PropMonitoringApplication.java
+++ src/main/java/lk/ijse/propmonitoring/PropMonitoringApplication.java
@@ -4,13 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity
 public class PropMonitoringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PropMonitoringApplication.class, args);
     }
+
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
